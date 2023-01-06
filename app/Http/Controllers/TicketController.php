235b5cb3c65eed1a487ce9995185069542457611
salmanaugmentler <salman@augmentler.com>
2023-01-06 11:47:53 +0500

    apis

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,26 +3,60 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ticket;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
     public function create(Request $request) {
         try {
-            $request->validate([
-                'title' => 'required'
-            ]);
+//            $request->validate([
+//                'title' => 'required'
+//            ]);
 
             $data = $request->all();
-
-            Ticket::create([
+            $description = $data['description'] ?? null;
+            $ticket = Ticket::create([
                'title' => $data['title'],
-               'description' => $data['description'] ?? null
+               'description' => $description
             ]);
+
+            if(isset($data['tasks'])) {
+                foreach ($data['tasks'] as $task) {
+                    if (trim($task) != '') {
+                        Todo::create([
+                            'ticket_id' => $ticket['id'],
+                            'task' => $task
+                        ]);
+                    }
+                }
+            }
+
+            $resp = Ticket::where('id', $ticket->id)->with('todos')->first();
+
+            return response(["ticket" => $resp], 200);
+
         }
         catch (\Exception $ex) {
-            return response(['message' => $ex], 400);
+            return response(['message' => $ex->getMessage()], 400);
         }
+    }
+
+    public function fetch(Request $request) {
+
+        try {
+            $resp = [];
+
+            $data = Ticket::with('todos')->get();
+
+            $resp["tickets"] = $data->toArray();
+
+            return response($resp, 200);
+        }
+        catch (\Exception $ex) {
+            return response(["message" => $ex->getMessage()], 400);
+        }
+
     }
 
     public function delete() {
